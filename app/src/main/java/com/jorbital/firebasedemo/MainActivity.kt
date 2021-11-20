b3e1a14ba2783df0deb2024ae647086f7bb42910
Jason <jasonctoms@gmail.com>
2021-11-20 10:28:31 +0100

    Add a feature flag

--- app/src/main/java/com/jorbital/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/jorbital/firebasedemo/MainActivity.kt
@@ -3,26 +3,39 @@ package com.jorbital.firebasedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.jorbital.firebasedemo.ui.theme.FirebaseDemoTheme
 
 class MainActivity : ComponentActivity() {
+    private val remoteConfig by lazy { Firebase.remoteConfig }
+    private var color = Color.Magenta
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FirebaseDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 10
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            val blue = remoteConfig.getBoolean("feature_flag_blue")
+            if (blue) color = Color.Blue
+            setContent {
+                FirebaseDemoTheme {
+                    FirebaseDemoScreen(color)
                 }
             }
         }
@@ -30,14 +43,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirebaseDemoScreen(color: Color) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Firebase Demo") }) },
+        content = { Greeting("Feature Flag Test", color) }
+    )
+}
+
+@Composable
+fun Greeting(name: String, color: Color) {
+    Box(Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .align(Alignment.Center)
+                .background(color)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(
+            text = "Hello $name!",
+            color = Color.White,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FirebaseDemoTheme {
-        Greeting("Android")
+        FirebaseDemoScreen(Color.Magenta)
     }
 }
